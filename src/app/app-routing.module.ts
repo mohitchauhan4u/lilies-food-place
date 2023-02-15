6bf015c575bfa0fb986c04f0e918d95912755190
mohitchauhan4u <mohitchauhan1929@gmail.com>
2023-02-15 21:28:10 +0530

    added components and their routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { HomePageComponent } from './views/home-page/home-page.component';
+import { LoginPageComponent } from './views/login-page/login-page.component';
+import { SignupPageComponent } from './views/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './views/signup-page/signup-page.component';
+import { LoginPageComponent } from './views/login-page/login-page.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { HomePageComponent } from './views/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent,
+    LoginPageComponent,
+    DashboardComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
