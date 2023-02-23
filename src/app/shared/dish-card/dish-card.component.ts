96cfe199c5d5d12fd2dcdcb7e1f91cd2aad79977
mohitchauhan4u <mohitchauhan1929@gmail.com>
2023-02-23 13:07:02 +0530

    added dish card

---
+++ src/app/shared/dish-card/dish-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-card',
+  templateUrl: './dish-card.component.html',
+  styleUrls: ['./dish-card.component.scss'],
+})
+export class DishCardComponent implements OnInit {
+  @Input() dish: any;
+
+  ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
+import { DishCardComponent } from './dish-card/dish-card.component';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [ButtonComponent, SidebarComponent],
-  declarations: [ButtonComponent, SidebarComponent],
+  exports: [ButtonComponent, SidebarComponent, DishCardComponent],
+  declarations: [ButtonComponent, SidebarComponent, DishCardComponent],
 })
 export class SharedModule {}
